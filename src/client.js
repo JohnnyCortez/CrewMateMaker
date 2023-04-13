95d524879ccf041dee132dc9e466548aff5604a2
Johnny Cortez <johnny.cortez@duke.edu>
2023-04-12 20:39:22 -0400

    connected supabase client

---
+++ src/client.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+const URL = "https://kzcmnfgluhvontlsshbz.supabase.co"
+
+const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt6Y21uZmdsdWh2b250bHNzaGJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODEzNDQ0OTcsImV4cCI6MTk5NjkyMDQ5N30.BGGZXDQuho4cue-xZ8QXeNmhK7e5FWNNKtkoPQXwlk4"
+
+export const supabase = createClient(URL, API_KEY);
